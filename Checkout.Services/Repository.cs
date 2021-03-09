325f9e12e54299190b59ef805f0101ede481c056
Ahmad Vegah <55131448@ad.mmu.ac.uk>
2021-03-09 09:16:01 +0000

    added repository to get item and check for special offer

---
+++ Checkout.Services/Repository.cs
@@ -0,0 +1,38 @@
+﻿using Checkout.Interfaces;
+using Checkout.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Checkout.Services
+{
+    public class Repository
+    {
+        private readonly IEnumerable<Item> _inventory;
+
+        public Repository(IDataSource dataSource)
+        {
+            _inventory = dataSource.Inventory;
+        }
+
+        public Item GetItem(string scannedItem)
+        {
+            Item item = _inventory
+                .Where(i => i.SKU == scannedItem)
+                .Select(i => i).SingleOrDefault();
+
+            if (item == null)
+            {
+                Console.WriteLine("Item does not exist");
+            }
+
+            return item;
+        }
+
+        public bool ContainsSpecialOffer(Item scannedItem)
+        {
+            return scannedItem.SpecialOffer != null;
+        }
+    }
+}
