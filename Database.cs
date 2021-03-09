a65f9960803e00a3f585ad411d7387bfdd17ea20
Ahmad Vegah <55131448@ad.mmu.ac.uk>
2021-03-09 07:22:23 +0000

    added database file for checkout items

---
+++ Database.cs
@@ -0,0 +1,22 @@
+﻿using Checkout.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checkout
+{
+    public class Database
+    {
+        public IEnumerable<Item> Inventory { get; set; }
+
+        public Database()
+        {
+            Inventory = new List<Item>
+                {
+                    new Item{SKU = "A99", Price = 0.50m},
+                    new Item{SKU = "B15", Price = 0.30m},
+                    new Item{SKU = "C40", Price = 0.60m},
+                };
+        }
+    }
+}
